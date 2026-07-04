906f42384ba49b59bb5898dcbd4953fcb290c147
Romadoni <rhomadoni3476@gmail.com>
2026-07-04 12:33:31 +0700

    Update code dan tambahkan gitignore

--- main.py
+++ main.py
@@ -84,6 +84,3 @@ def get_sus_statistics(current_user: str = Depends(get_current_user)):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
